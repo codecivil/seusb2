372f849308c9dbb533db83cb34645c6e47969295
Marco Kühnel <kuehnel@codecivil.de>
2017-04-19 14:13:43 +0200

    Add files via upload

---
+++ 1.0/debian/seusb2-1.0/acpi-powerbtn-seusb2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# acpi-powerbtn-seusb2.sh
+#
+
+# close the dialogues
+pkill zenity
+
+# read config
+. /etc/seusb2/seusb2.conf
+
+function _confirm() {
+	FILENAME="$1"
+	###add allow udev rule
+	echo "seusb2_allow=true" >> $MSGDIR/$FILENAME
+	zenity --notification --title="${MSG['TITLE',$MSGLANG]}" --text="${MSG['REATTACH',$MSGLANG]}"
+}
+
+#process the queue
+for _FILE in $(ls $MSGDIR); do _confirm $_FILE; done
